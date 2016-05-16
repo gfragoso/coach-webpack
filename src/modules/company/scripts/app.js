f57fd2e0d4d45e3375db4977649e1ad367ecc613
Gustavo Fragoso <gustavo.fragoso@fcamara.com.br>
2016-05-16 18:20:09 -0300

    correcoes

--- src/modules/company/scripts/app.js
+++ src/modules/company/scripts/app.js
@@ -1,10 +1,5 @@
 var app = angular.module('company', []);
 
-var configContext = require.context('./configs', false, /config.js/);
-configContext.keys().forEach(function(item) {
-    configContext(item)(app);
-});
-
 var controllerContext = require.context('./controllers', false, /controller.js/);
 controllerContext.keys().forEach(function(item) {
     controllerContext(item)(app);
